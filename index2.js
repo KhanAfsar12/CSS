70cbe113a9d97b2a3b68c7484b540ea3c6ff40be
KhanAfsar12 <ka484564@gmail.com>
2024-04-17 17:35:15 +0530

    Grid completed

--- index2.js
+++ index2.js
@@ -1,1 +1,18 @@
-console.log("HI");
+if(Math.random() > 0.5){
+    var y = 5;
+}
+console.log(y);
+
+console.log(x===undefined);
+var x = 3;
+
+(function(){
+    console.log(x);
+    var x = "local value";
+})();
+
+if(true){
+var a;
+}
+console.log(a);
+a = 10
